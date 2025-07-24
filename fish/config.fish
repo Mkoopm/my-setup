ff9b0c959a11d2e2a117a5126dbf3135628c3a13
Marten Koopmans <36400337+Mkoopm@users.noreply.github.com>
2025-07-24 10:54:18 +0200

    feat: add fish config and remove complicated init command from tmux

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,30 @@
-
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
 if test -f /Applications/Anaconda3/anaconda3/bin/conda
-    eval /Applications/Anaconda3/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+    eval /Applications/Anaconda3/anaconda3/bin/conda "shell.fish" hook $argv | source
 end
 # <<< conda initialize <<<
 
+set LSCOLORS exfxcxdxbxegedabagacad
+
+set -gx PYENV_ROOT "$HOME/.pyenv"
+command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+
+# Created by `pipx` on 2023-12-18 10:49:39
+export PATH="/Users/mk/.local/bin:$PATH"
+
+alias gs='git status'
+alias gaa='git add *'
+alias gl="git log --graph --oneline --decorate --all"
+
+export EDITOR=nano
+export VISUAL=nvim
+
+zoxide init fish | source
+
+fish_add_path "HOME/.bin"
+
+### MANAGED BY RANCHER DESKTOP START (DO NOT EDIT)
+fish_add_path "$HOME/.rd/bin"
+### MANAGED BY RANCHER DESKTOP END (DO NOT EDIT)
