1dae0bcf79da488bb0852eabe6df87ea2dbbc4e6
Marten Koopmans <36400337+Mkoopm@users.noreply.github.com>
2025-07-24 10:21:52 +0200

    feat: add fish config

---
+++ fish/config.fish
@@ -0,0 +1,8 @@
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /Applications/Anaconda3/anaconda3/bin/conda
+    eval /Applications/Anaconda3/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+end
+# <<< conda initialize <<<
+
